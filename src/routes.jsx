d40afedc2422025d84bf1e1b94a0df5b07216732
jin <jin337x@foxmail.com>
2026-03-20 16:27:03 +0800

    文件修改

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import Home from 'src/views/index.jsx'
+import Home from './views/index.jsx'
 
-import CssDocs from 'src/pages/CssDocs.jsx'
-import CssGradients from 'src/pages/CssGradients.jsx'
-import Flexbox from 'src/pages/Flexbox.jsx'
-import Triangle from 'src/pages/Triangle.jsx'
-import Waterfall from 'src/pages/Waterfall.jsx'
+import CssDocs from './pages/CssDocs.jsx'
+import CssGradients from './pages/CssGradients.jsx'
+import Flexbox from './pages/Flexbox.jsx'
+import Triangle from './pages/Triangle.jsx'
+import Waterfall from './pages/Waterfall.jsx'
 
 export const router = createBrowserRouter([
   {
